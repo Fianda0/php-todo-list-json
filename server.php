81bceeceb4ffff0afdf42faecaa8c0e9d9ae0311
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-06-14 15:13:35 +0200

    Spostamento array in file jason

--- server.php
+++ server.php
@@ -1,31 +1,5 @@
 <?php
 
-$toDO = [
-    [
-        'orario' => '08:30',
-        'azione' => 'scegliarsi'
-    ],
-    [
-        'orario' => '09:30',
-        'azione' => 'iniziare lezione boolean'
-    ],
-    [
-        'orario' => '11:00',
-        'azione' => 'fare pausa caffè'
-    ],
-    [
-        'orario' => '13:00',
-        'azione' => 'pranzare'
-    ],
-    [
-        'orario' => '15:00',
-        'azione' => 'svolgere esercizi'
-    ],
-    [
-        'orario' => '17:00',
-        'azione' => 'andare a correre'
-    ]
-];
 
 header('Content-Type: application/json');
 
